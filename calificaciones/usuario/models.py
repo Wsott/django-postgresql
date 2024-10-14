2f76b395402371348bc29fd9ed98f92d84c04fda
Emiliano Gomez Olivera <znasters401@gmail.com>
2024-10-14 18:53:11 -0300

    Creada funciones asociadas con el nombre: set, get y update.
    Ademas se creo los tests correspondientes.

--- calificaciones/usuario/models.py
+++ calificaciones/usuario/models.py
@@ -9,8 +9,11 @@ class Usuario(models.Model):
     privados y solamente accedibles mediante los metodos correspondientes del tipo get_[atributo].
     Estos metodos permiten acceder de forma correcta a la informacion.
 
-    Similar a lo anterior, para cambiar la informacion almacenada del registro es necesario utilizar
+    Similar a lo anterior, para definir la informacion almacenada del registro es necesario utilizar
     los metodos set_[atributo] y pasarle los parametros correctos.
+
+    Para actualizar los datos, sera necesario utilizar los metodos update_[atributo] y pasarle los datos
+    correspondientes.
 
     :ivar nombre: Nombre de usuario, longitud maxima de 32 caracteres
     :vartype nombre: str
@@ -25,3 +28,68 @@ class Usuario(models.Model):
     _contrasenna = models.CharField(max_length=256)
     _fecha_creacion = models.DateField(default=timezone.now().date(), editable=False)
     _ultimo_login = models.DateField(default=None, null=True)
+
+    def _validar_nombre(self, nombre):
+        """
+        Funcion de uso interno para validar que el nombre de usuario se adecua a lo necesario.
+
+        Debera: ser str, no tener mas de 32 caracteres, no tener espacios en blanco.
+
+        :type nombre: str
+        :param nombre: Valor que sera evaluado.
+
+        :rtype: bool
+        :return: Devuelve True si es valido o False si es invalido
+        """
+
+        if isinstance(nombre, str):
+            if len(nombre) < 32:
+                if not ' ' in nombre:
+                    self._nombre = nombre
+                    return True
+
+        return False
+
+    def set_nombre(self, nombre):
+        """
+        Recibe una cadena de texto que sera el nombre de usuario. Solamente se puede realizar el set
+        la primera vez, posterioremente es necesario utilizar la funcion update_nombre
+
+        :type nombre: str
+        :param nombre: Nombre del usuario.
+
+        :rtype: bool
+        :return: Devuelve True si la operacion se completo con exito. False si la operacion fallo.
+        """
+
+        if not self._nombre and self._validar_nombre(nombre):
+            self._nombre = nombre
+            return True
+
+        return False
+
+    def get_nombre(self):
+        """
+        Devuelve el nombre de usuario del registro representado por la instancia.
+
+        :rtype: str | None
+        """
+
+        return self._nombre if self._nombre is not '' else None
+
+    def update_nombre(self, nombre):
+        """
+        Actualiza el nombre de usuario previamente definido.
+
+        :type nombre: str
+        :param nombre: Valor al cual se actualizara el nombre de usuario.
+
+        :rtype: bool
+        :return: Devuelve True si la operacion se completo con exito o False si fallo.
+        """
+
+        if self._nombre and self._validar_nombre(nombre):
+            self._nombre = nombre
+            return True
+
+        return False
--- calificaciones/usuario/tests.py
+++ calificaciones/usuario/tests.py
@@ -1,3 +1,86 @@
 from django.test import TestCase
 
+from .models import Usuario
+
+
 # Create your tests here.
+class UsuarioClassTestCase(TestCase):
+    def setUp(self):
+        self.user_test = Usuario()
+
+    def test_creacion_usuario(self):
+        self.assertIsInstance(self.user_test, Usuario,
+                              'Se debia generar una instancia de Usuario pero es None')
+
+    def test_set_nombre(self):
+        self.assertFalse(self.user_test.set_nombre(123),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.set_nombre(1.5),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.set_nombre(Usuario()),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.set_nombre(None),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.set_nombre(True),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.set_nombre(b'String'),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.set_nombre('Este string tiene espacios'),
+                         'Debia retornar False: el string no puede tener espacios')
+
+        self.assertFalse(self.user_test.set_nombre('1234567890_1234567890_1234567890_'),
+                         'Debia retornar False: el string tiene mas de 32 caracteres')
+
+        self.assertTrue(self.user_test.set_nombre('LoremIpsum'),
+                        'Deberia retornar True: el string es valido')
+
+        self.assertFalse(self.user_test.set_nombre('NuevoNombre'),
+                         'Deberia retornar False: ya se ha definido un nombre de usuario anteriormente')
+
+    def test_get_nombre(self):
+        self.user_test.set_nombre('LoremIpsum')
+
+        self.assertIsNone(Usuario().get_nombre(),
+                          'Deberia ser None: no deberia haber ningun nombre de usuario asociado')
+
+        self.assertIsInstance(self.user_test.get_nombre(), str,
+                              'Deberia ser str: se esperaba que devuelva un str')
+
+        self.assertEquals(self.user_test.get_nombre(), 'LoremIpsum',
+                          'Deberia ser LoremIpsum: se esperaba que el nombre de usuario coincida con el guardado')
+
+    def test_update_nombre(self):
+        self.user_test.set_nombre('LoremIpsum')
+
+        self.assertFalse(self.user_test.update_nombre(123),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.update_nombre(1.5),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.update_nombre(Usuario()),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.update_nombre(None),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.update_nombre(True),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.update_nombre(b'String'),
+                         'Debia retornar False: solo se acepta str')
+
+        self.assertFalse(self.user_test.update_nombre('Este string tiene espacios'),
+                         'Debia retornar False: el string no puede tener espacios')
+
+        self.assertFalse(self.user_test.update_nombre('1234567890_1234567890_1234567890_'),
+                         'Debia retornar False: el string tiene mas de 32 caracteres')
+
+        self.assertTrue(self.user_test.update_nombre('NuevoNombre'),
+                        'Deberia retornar True: el string es valido')
