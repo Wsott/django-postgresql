12901aee06580f4c07b9c7c43daa2ac028b526ff
Emiliano Gomez Olivera <znasters401@gmail.com>
2024-10-16 15:04:18 -0300

    Agregado mensajes de error al iniciar sesion

--- calificaciones/usuario/views.py
+++ calificaciones/usuario/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 
 from core.forms import SignInForm, LogInForm
 from usuario.models import Usuario
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
@@ -26,19 +27,27 @@ def registrarse(request):
 
 
 def login(request):
+    mensaje_error = None
+
     if request.method == 'POST':
         datos = LogInForm(request.POST)
 
         if datos.is_valid():
-            usuario = Usuario.objects.get(_nombre=datos.cleaned_data['nombre'])
-            if usuario and usuario.iniciar_sesion(datos.cleaned_data['contrasenna']):
-                return HttpResponse('Iniciada sesion')
-            else:
-                return HttpResponse('Error al iniciar sesion')
+            try:
+                usuario = Usuario.objects.get(_nombre=datos.cleaned_data['nombre'])
+                if usuario and usuario.iniciar_sesion(datos.cleaned_data['contrasenna']):
+                    return HttpResponse('Iniciada sesion')
+                else:
+                    mensaje_error = 'Error al iniciar sesion, verifique que su usario y contraseña sean correctos'
+            except ObjectDoesNotExist:
+                mensaje_error = 'Error al iniciar sesion, verifique que su usario y contraseña sean correctos'
+
+
         else:
-            return HttpResponse('Error')
+            mensaje_error = 'Verifique que el usuario y la contraseña no tengan espacios'
 
     return render(request, 'login.html', {
         'titulo': 'Iniciar sesion',
-        'form': LogInForm()
+        'form': LogInForm(),
+        'error': mensaje_error
     })
