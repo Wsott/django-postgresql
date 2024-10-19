5c6192500b65fad48a3bee2f9c20e76fb7752c38
Emiliano Gomez Olivera <znasters401@gmail.com>
2024-10-18 22:59:50 -0300

    Agregado el template de perfil y su url.
    TODO: completar la funcionalidad del template pasandole los datos reales que utilizaria

--- calificaciones/usuario/urls.py
+++ calificaciones/usuario/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('login', views.login, name='login'),
     path('signin', views.registrarse, name='signin'),
+    path('perfil', views.perfil, name='perfil')
 ]
